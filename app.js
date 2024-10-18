a705b921a7f37eb4ed6fe5d460f1d4f47d1a745c
joy1135 <102030495+joy1135@users.noreply.github.com>
2024-10-18 20:00:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
     if('serviceWorker' in navigator){
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('SW registered', registration)
             })
