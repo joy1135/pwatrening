d01af855eadae750aa207a5177eac0d8df2f4368
joy1135 <102030495+joy1135@users.noreply.github.com>
2024-10-18 19:43:41 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const staticCacheName = 'site-static-v1'
+const assets = [
+    '.',
+    'imt.html',
+    'app.js',
+    'logo.png',
+    'style.css',
+    'app-image/images/icons/icon-128x128.png',
+    'app-image/images/icons/icon-192x192.png'
+]
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('Кеширование ресурсов')
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)
+        })
+    )
+})
